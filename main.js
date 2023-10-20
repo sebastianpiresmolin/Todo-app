de956be76ba2124d4ef30148af6a4bcb3cca9447
sebastianpiresmolin <piresmolinsecure@gmail.com>
2023-10-20 10:59:40 +0200

    format document

--- main.js
+++ main.js
@@ -1,69 +1,64 @@
-
 // I only have one <form> in my html so i can use it as my selector
 //add eventlistener to look for click on check and/or delete button
-document.querySelector('form').addEventListener("submit", handleSubmitForm);
-document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
+document.querySelector("form").addEventListener("submit", handleSubmitForm);
+document
+  .querySelector("ul")
+  .addEventListener("click", handleClickDeleteOrCheck);
 //Here I store every string.
 const todoArray = [];
 let completedTodos = 0;
-
-
 
 /*
 Calling preventDefault() because I don't want the page to
 reload at every submisson. Here i add alert if user tries passing null.
 */
 function handleSubmitForm(e) {
-    e.preventDefault();
-    let input = document.querySelector('#input');
-    let errorText = document.querySelector('#errorText');
+  e.preventDefault();
+  let input = document.querySelector("#input");
+  let errorText = document.querySelector("#errorText");
 
-    if (input.value !== '') {
-        addTodo(input.value);
-        todoArray.push(input.value);
-        input.value = ''; // Clear the input field
-        errorText.textContent = ''; // Clear the error message
-    } else {
-        errorText.textContent = 'Input must not be empty'; // Display the error message
-        errorText.classList.add('flash-three-times'); // Add the flashing animation class
-        setTimeout(() => {
-            errorText.classList.remove('flash-three-times'); 
-        }, 900);
-    }
+  if (input.value !== "") {
+    addTodo(input.value);
+    todoArray.push(input.value);
+    input.value = ""; // Clear the input field
+    errorText.textContent = ""; // Clear the error message
+  } else {
+    errorText.textContent = "Input must not be empty"; // Display the error message
+    errorText.classList.add("flash-three-times"); // Add the flashing animation class
+    setTimeout(() => {
+      errorText.classList.remove("flash-three-times");
+    }, 900);
+  }
 }
-
-
 
 // I use .name since I have already declared button names
 function handleClickDeleteOrCheck(e) {
-    const listItem = e.target.closest('li');
-    
-    if (listItem) {
-        //parent 'li' element
-        const spanElement = listItem.querySelector('span');
-        if (e.target === spanElement) {
-            // Handle the click on the 'span' element
-        }
-        else if (e.target.name === 'deleteButton') {
-            deleteTodo(e);
-        }
+  const listItem = e.target.closest("li");
+
+  if (listItem) {
+    //parent 'li' element
+    const spanElement = listItem.querySelector("span");
+    if (e.target === spanElement) {
+      // Handle the click on the 'span' element
+    } else if (e.target.name === "deleteButton") {
+      deleteTodo(e);
     }
+  }
 }
 
 function addTodo(todo) {
+  let ul = document.querySelector("ul"); //my complete list
+  let li = document.createElement("li"); //item on the list
 
-    let ul = document.querySelector('ul'); //my complete list
-    let li = document.createElement('li'); //item on the list
+  //Add span check and delete icon buttons to every todo-item
+  //Add class todo-list-item to every item and append to li
 
-//Add span check and delete icon buttons to every todo-item
-//Add class todo-list-item to every item and append to li
-
-    li.innerHTML = `
+  li.innerHTML = `
         <span class="todo-item">${todo}</span>
         <button name="deleteButton"><i class="fa fa-trash"></i></button>
     `;
-    li.classList.add('todo-list-item');
-    ul.appendChild(li);
+  li.classList.add("todo-list-item");
+  ul.appendChild(li);
 }
 
 /*
@@ -77,32 +72,30 @@ completedTodos variable. I call the updateCompletedTodosCount function to update
 the display of the counter.
 */
 
-document.querySelector('#todoList').addEventListener('click', function (e) {
-    if (e.target.tagName === 'SPAN') {
-        const listItem = e.target.closest('li');
-        if (listItem) {
-            if (listItem.classList.contains('completed')) {
-                listItem.classList.remove('completed');
-                completedTodos--;
-            } else {
-                listItem.classList.add('completed');
-                completedTodos++;
-            }
-            updateCompletedTodosCount();
-        }
+document.querySelector("#todoList").addEventListener("click", function (e) {
+  if (e.target.tagName === "SPAN") {
+    const listItem = e.target.closest("li");
+    if (listItem) {
+      if (listItem.classList.contains("completed")) {
+        listItem.classList.remove("completed");
+        completedTodos--;
+      } else {
+        listItem.classList.add("completed");
+        completedTodos++;
+      }
+      updateCompletedTodosCount();
     }
+  }
 });
-
 
 // Function to update and display completed todos count
 function updateCompletedTodosCount() {
-    let completedTodosCount = document.getElementById('completedTodosCount');
-    completedTodosCount.innerHTML = `${completedTodos} completed`;
+  let completedTodosCount = document.getElementById("completedTodosCount");
+  completedTodosCount.innerHTML = `${completedTodos} completed`;
 }
 
 // Call this function to initialize the display
 updateCompletedTodosCount();
-
 
 /*
 The logic on this funtion is very similar to checkTodo(), but i just pass
@@ -113,12 +106,11 @@ text content and finally remove the item from the todoArray
 */
 
 function deleteTodo(e) {
-    let item = e.target.parentNode;
-    let todoText = item.querySelector('.todo-item').textContent;
-    var index = todoArray.indexOf(todoText); 
-    if (index !== -1) {
-        todoArray.splice(index, 1);
-    }
-    item.remove();
+  let item = e.target.parentNode;
+  let todoText = item.querySelector(".todo-item").textContent;
+  var index = todoArray.indexOf(todoText);
+  if (index !== -1) {
+    todoArray.splice(index, 1);
+  }
+  item.remove();
 }
-
